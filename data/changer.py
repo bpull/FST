c152d5a42814763a9363f8016512eece1837c563
Micky Hall <m172412@usna.edu>
2017-02-08 09:14:59 -0800

    corrected changer to work with a years worth of data

--- data/changer.py
+++ data/changer.py
@@ -3,16 +3,17 @@ import re
 
 fp = open(sys.argv[1],"r")
 
-all_data = fp.readline()
-all_data = re.sub('[\[\](]','',all_data)
-all_data = all_data.replace('),','#')
-all_data = all_data.replace(')','')
-split_data = all_data.split("#")
-for all_info in split_data:
-    split_info = all_info.split(",")
-    for info in split_info:
-        printer_info = info.split(".")
-        if info is split_info[-1]:
-            print(printer_info[0])
-        else:
-            print(printer_info[0],end=",")
+for line in fp:
+    #all_data = fp.readline()
+    all_data = re.sub('[\[\](]','',line)
+    all_data = all_data.replace('),','#')
+    all_data = all_data.replace(')','')
+    split_data = all_data.split("#")
+    for all_info in split_data:
+        split_info = all_info.split(",")
+        for info in split_info:
+            printer_info = info.split(".")
+            if info is split_info[-1]:
+                print(printer_info[0])
+            else:
+                print(printer_info[0],end=",")
