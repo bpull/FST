535e765e1795fe0f1a0ffb050d42daffdcdc1408
Micky Hall <m172412@usna.edu>
2017-04-10 20:52:04 -0700

    a shit ton of csv files

--- data/stockdata/floatchanger.py
+++ data/stockdata/floatchanger.py
@@ -1,29 +1,48 @@
 import sys
 import re
+import os
 
 #fp = open(sys.argv[1],"r")
-company = "A"
-for i in range(2007,2017,1):
-    directory = company+"/"+str(i)+"-data"
-    for j in range(1,13,1):
-        filename = directory+"/"+str(j)
-        try:
-            fp = open(filename,"r")
-            all_data = fp.readline()
-            all_data = re.sub('[\[\](]','',all_data)
-            all_data = all_data.replace('),','#')
-            all_data = all_data.replace(')','')
-            split_data = all_data.split("#")
-            print filename
-            counter = 1
-            for info in split_data:
-                print info.strip() + ", " +str(counter)
-                counter+= 1
-
-
-            fp.close()
-        except:
-            pass
+# company = "A"
+# newfile = company+"/"+company+".csv"
+# nf = open(newfile,"w")
+# for directory in os.listdir("."):
+#     if os.path.isdir(directory):
+#         newfile = directory.strip()+"/"+directory.strip()+".csv"
+#         nf = open(newfile,"w")
+#         for i in range(2007,2017,1):
+#             directory = directory+"/"+str(i)+"-data"
+#             for j in range(1,13,1):
+#                 filename = directory+"/"+str(j)
+#                 try:
+#                     fp = open(filename,"r")
+#                     all_data = fp.readline()
+#                     all_data = re.sub('[\[\](]','',all_data)
+#                     all_data = all_data.replace('),','#')
+#                     all_data = all_data.replace(')','')
+#                     split_data = all_data.split("#")
+#                     print filename
+#                     counter = 1
+#                     for info in split_data:
+#                         tofile = info.strip() + ", " +str(j) +"-"+str(counter)+"-"+str(i)+"\n"
+#                         nf.write(tofile)
+#                         counter+= 1
+#                     fp.close()
+#                 except:
+#                     pass
+#         nf.close()
+cots = 0
+for direct in os.listdir("."):
+    if os.path.isdir(direct):
+        cots += 1
+        print direct
+        flag = 0
+        for item in os.listdir(direct):
+            if ".csv" in item:
+                flag = 1
+        if flag == 0:
+            print "no csv for " +direct
+print cots
 '''
 for line in fp:
     #all_data = fp.readline()
